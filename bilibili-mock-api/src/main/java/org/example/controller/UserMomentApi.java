eea8f67ea86487b7fbd76793529c12bec020eb51
Alex <alex@q-83.com>
2023-03-19 21:10:48 +1100

    添加了获取用户的信息的方法

--- bilibili-mock-api/src/main/java/org/example/controller/UserMomentApi.java
+++ bilibili-mock-api/src/main/java/org/example/controller/UserMomentApi.java
@@ -10,9 +10,12 @@ import org.example.helpers.UserVerifyTokenHelper;
 import org.example.service.UserMomentService;
 import org.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserMomentApi {
@@ -41,5 +44,21 @@ public class UserMomentApi {
 
         return JsonResponse.success("success");
     }
+
+    // 获取用户关注的人的moment
+    @GetMapping("/moments/get-user-subscribe-moment")
+    public JsonResponse<List<UserMoment>>getUserSubscribedMoment() {
+        long userId;
+        boolean developmentMode = appConfig.isDevelopment();
+        if (developmentMode) {
+            userId = 17;
+        } else {
+            userId = userVerifyTokenHelper.getCurrentUserIdByToken();
+        }
+
+        List<UserMoment> userMoments = userMomentService.getUserSubscribedMomentByUserId(userId);
+        return new JsonResponse(userMoments);
+    }
+
 
 }
--- bilibili-mock-service/src/main/java/org/example/service/UserMomentService.java
+++ bilibili-mock-service/src/main/java/org/example/service/UserMomentService.java
@@ -1,5 +1,6 @@
 package org.example.service;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
@@ -12,10 +13,12 @@ import org.example.domain.UserMoment;
 import org.example.utils.RocketMqUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class UserMomentService {
@@ -25,6 +28,9 @@ public class UserMomentService {
 
     @Autowired
     private ApplicationContext applicationContext;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
     public void addMoment(UserMoment userMoment) throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
         userMoment.setCreateTime(new Date());
@@ -38,5 +44,14 @@ public class UserMomentService {
                 JSONObject.toJSONString(userMoment).getBytes(StandardCharsets.UTF_8)
         );
         RocketMqUtils.sendSyncMessage(defaultMQProducer, msg);
+    }
+
+    public List<UserMoment> getUserSubscribedMomentByUserId(long userId) {
+        String key = "subscribe:" + userId;
+        String subscribedList = redisTemplate.opsForValue().get(key);
+
+        List<UserMoment> userMomentsList = JSONArray.parseArray(subscribedList, UserMoment.class);
+
+        return userMomentsList;
     }
 }
